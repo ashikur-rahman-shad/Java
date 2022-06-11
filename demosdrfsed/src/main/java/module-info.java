b488f15e6c7e7f7f0acbf7074496ed2c4bedfba0
shad <shadgood@gmail.com>
2022-06-12 00:14:25 +0600

    Push

--- demosdrfsed/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demosdrfsed {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demosdrfsed to javafx.fxml;
-    exports com.example.demosdrfsed;
-}
